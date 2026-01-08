4f4d96737df081e9d264e700f41fffc79728e8bc
AmbilyThomas90 <ambilymariya.9@gmail.com>
2026-01-08 16:28:51 +0530

    Project updated

--- controllers/appointmentController.js
+++
@@ -1,6 +0,0 @@
-import Appointment from "../models/Appointment.js";
-
-export const bookAppointment = async (req, res) => {
-  const appointment = await Appointment.create(req.body);
-  res.json(appointment);
-};
