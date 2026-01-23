fbcbe0f25889346fb21ff2a38061c540c5b30e72
AmbilyThomas90 <ambilymariya.9@gmail.com>
2026-01-23 16:19:41 +0530

    test user nutrition

--- Server/routes/nutritionRoutes.js
+++ Server/routes/nutritionRoutes.js
@@ -15,7 +15,7 @@ router.post(
   createNutrition
 );
 // Get user nutrition --By User
-router.get("/user-nutrition", protect, getUserNutrition);
+router.get("/user-nutrition", protect, authorizeRole("user"), getUserNutrition);
 // // User / Trainer get nutrition by user
 // router.get(
 //   "/user-nutrition/:userId",
